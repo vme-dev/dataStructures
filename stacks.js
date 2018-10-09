72790f1f3defa9797e276343c861bb6258ed430a
vme-dev <v.e.malevaniy@gmail.com>
2018-10-10 00:01:05 +0300

    add stack

---
+++ stacks.js
@@ -0,0 +1,34 @@
+var Stack = function () {
+    this.count = 0;
+    this.storage = [];
+
+    this.push = function (value) {
+        this.storage[this.count] = value;
+        this.count++; 
+    }
+
+    this.pop = function () {
+        if (this.count === 0) {
+            return undefined;
+        }
+
+        this.count--;
+        var result = this.storage[this.count];
+        delete this.storage[this.count];
+        return result;
+    }
+
+    this.size = function () {
+        return this.count;
+    }
+
+    this.peek = function () {
+        return this.storage[this.count-1];
+    }
+}
+
+var myStack = new Stack();
+
+myStack.push(1);
+myStack.push(3);
+console.log ( myStack.peek() );
